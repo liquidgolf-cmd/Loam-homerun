2db5ae625351ea54ec6dd73ec8fcbe6180a0411b
liquidgolf-cmd <liquidgolf@gmail.com>
2025-12-10 16:48:31 -0700

    Move AIDifferentiator section above Services section

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <Services />
       <AIDifferentiator />
+      <Services />
       <WhyLoam />
       <HomerunMethod />
       <Portfolio />
